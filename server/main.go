b8b4f1312cd81e122759b4b456c1e80eb16d6fbb
kmiit <61952405+kmiit@users.noreply.github.com>
2024-10-10 05:09:15 +0800

    Specify HTTP config

--- server/main.go
+++ server/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+    "net/http"
+    "time"
+    "github.com/gin-gonic/gin"
 )
 
 func main () {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
 	    c.JSON(200, gin.H{
 	        "message": "pong",
 	    })
 	})
-	r.Run()
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
 }
