8ad36ce37e2bd60664cce0393653bcc801eafa16
kmiit <kmiit007@gmail.com>
2024-10-15 23:08:04 +0800

    Watch event on file changing

--- server/utils/storage/watcher.go
+++ server/utils/storage/watcher.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"fmt"
+
 	"github.com/fsnotify/fsnotify"
 	"github.com/kmiit/vivi/utils/log"
 )
@@ -20,4 +22,35 @@ func WatchStorage() {
 			log.I(TAG, "Watching: ", p)
 		}
 	}
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.I(TAG, "Event: ", event)
+				fmt.Print(event.String())
+				switch {
+				case event.Has(fsnotify.Write):
+					log.I(TAG, "Modified file: ", event.Name)
+				case event.Has(fsnotify.Create):
+					log.I(TAG, "Created file: ", event.Name)
+				case event.Has(fsnotify.Remove):
+					log.I(TAG, "Removed file: ", event.Name)
+				case event.Has(fsnotify.Rename):
+					log.I(TAG, "Renamed file: ", event.Name)
+				case event.Has(fsnotify.Chmod):
+					log.I(TAG, "Changed permission file: ", event.Name)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.E(TAG, "Error: ", err)
+			}
+		}
+	}()
+	<-make(chan struct{})
 }
