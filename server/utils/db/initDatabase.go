4bd09af44e5532d03fe83935afb200810063cc48
kmiit <kmiit007@gmail.com>
2024-10-16 00:12:15 +0800

    Export redis client

--- server/utils/db/initDatabase.go
+++ server/utils/db/initDatabase.go
@@ -1,8 +1,6 @@
 package db
 
 import (
-	"context"
-
 	"github.com/kmiit/vivi/utils/config"
 	"github.com/kmiit/vivi/utils/log"
 	"github.com/redis/go-redis/v9"
@@ -10,21 +8,15 @@ import (
 
 const TAG = "Database"
 
+var RDB *redis.Client
+
 func InitDatabase() {
 	address := config.DatabaseConfig.DbAddress + ":" + config.DatabaseConfig.DbPort
 
-	rdb := redis.NewClient(&redis.Options{
+	RDB = redis.NewClient(&redis.Options{
 		Addr:     address,
 		Password: config.DatabaseConfig.DbPassword,
 		DB:       config.DatabaseConfig.DbNumber,
 	})
-	log.I(TAG, "Connected to Redis", rdb)
-
-	ctx := context.Background()
-	r, err := rdb.Ping(ctx).Result()
-	if err != nil {
-		log.F(TAG, err)
-	} else {
-		log.I(TAG, "Ping: ", r)
-	}
+	log.I(TAG, "Connected to Redis", RDB)
 }
